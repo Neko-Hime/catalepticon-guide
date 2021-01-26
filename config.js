7baef0f2e249a51033da87d05fa16ac75acbe891
Michał Walewski <mk.walewski@gmail.com>
2021-01-26 22:09:19 +0100

    .gitignore and fixes

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const ITEM_BOMB = 1001;
 const ITEM_FOR_LINE = 553;
-const ITEM_FOR_CIRCLE = 553;
+const ITEM_FOR_CIRCLE = 912;
 
 module.exports = {
     EVENT_MESSAGE_TYPE: 42,
@@ -9,10 +9,14 @@ module.exports = {
             1000: [
                 //Line Forward + Side Lines
                 {id: 107, msg: '', spawns: [
-                    {type: 'line', pointDegrees: 90, pointRadius: 400, item: ITEM_FOR_LINE, time: 2000, degrees: 0, intervalRadius: 50, maxRadius: 1000},
-                    {type: 'line', pointDegrees: 90, pointRadius: 400, item: ITEM_FOR_LINE, time: 2000, degrees: 180, intervalRadius: 50, maxRadius: 1000},
-                    {type: 'line', pointDegrees: 270, pointRadius: 400, item: ITEM_FOR_LINE, time: 2000, degrees: 0, intervalRadius: 50, maxRadius: 1000},
-                    {type: 'line', pointDegrees: 270, pointRadius: 400, item: ITEM_FOR_LINE, time: 2000, degrees: 180, intervalRadius: 50, maxRadius: 1000}
+                    {type: 'line', pointDegrees: 90, pointRadius: 50, item: ITEM_FOR_LINE, time: 3000, degrees: 0, intervalRadius: 50, maxRadius: 500},
+                    {type: 'line', pointDegrees: 90, pointRadius: 50, item: ITEM_FOR_LINE, time: 3000, degrees: 180, intervalRadius: 50, maxRadius: 500},
+                    {type: 'line', pointDegrees: 90, pointRadius: 400, item: ITEM_FOR_LINE, time: 3000, degrees: 0, intervalRadius: 50, maxRadius: 1000},
+                    {type: 'line', pointDegrees: 90, pointRadius: 400, item: ITEM_FOR_LINE, time: 3000, degrees: 180, intervalRadius: 50, maxRadius: 1000},
+                    {type: 'line', pointDegrees: 270, pointRadius: 50, item: ITEM_FOR_LINE, time: 3000, degrees: 0, intervalRadius: 50, maxRadius: 500},
+                    {type: 'line', pointDegrees: 270, pointRadius: 50, item: ITEM_FOR_LINE, time: 3000, degrees: 180, intervalRadius: 50, maxRadius: 500},
+                    {type: 'line', pointDegrees: 270, pointRadius: 400, item: ITEM_FOR_LINE, time: 3000, degrees: 0, intervalRadius: 50, maxRadius: 1000},
+                    {type: 'line', pointDegrees: 270, pointRadius: 400, item: ITEM_FOR_LINE, time: 3000, degrees: 180, intervalRadius: 50, maxRadius: 1000}
                 ], timeout: 0},
                 //Inner + Outer AOEs
                 {id: 114, msg: '', spawns: [
@@ -20,26 +24,26 @@ module.exports = {
                 ], timeout: 0},
                 //Pizza
                 {id: 127, msg: '', spawns: [
-                    {type: 'line', pointDegrees: 0, pointRadius: 0, item: ITEM_FOR_LINE, time: 2000, degrees: 160, intervalRadius: 50, maxRadius: 600},
-                    {type: 'line', pointDegrees: 0, pointRadius: 0, item: ITEM_FOR_LINE, time: 2000, degrees: 200, intervalRadius: 50, maxRadius: 600}
+                    {type: 'line', pointDegrees: 0, pointRadius: 0, item: ITEM_FOR_LINE, time: 2000, degrees: 160, intervalRadius: 50, maxRadius: 650},
+                    {type: 'line', pointDegrees: 0, pointRadius: 0, item: ITEM_FOR_LINE, time: 2000, degrees: 200, intervalRadius: 50, maxRadius: 650}
                 ], timeout: 0},
                 //Pizza
                 {id: 128, msg: '', spawns: [
-                    {type: 'line', pointDegrees: 0, pointRadius: 0, item: ITEM_FOR_LINE, time: 2000, degrees: 160, intervalRadius: 50, maxRadius: 600},
-                    {type: 'line', pointDegrees: 0, pointRadius: 0, item: ITEM_FOR_LINE, time: 2000, degrees: 200, intervalRadius: 50, maxRadius: 600}
+                    {type: 'line', pointDegrees: 0, pointRadius: 0, item: ITEM_FOR_LINE, time: 2000, degrees: 160, intervalRadius: 50, maxRadius: 650},
+                    {type: 'line', pointDegrees: 0, pointRadius: 0, item: ITEM_FOR_LINE, time: 2000, degrees: 200, intervalRadius: 50, maxRadius: 650}
                 ], timeout: 0},
                 //Pizza
                 {id: 148, msg: '', spawns: [
-                    {type: 'line', pointDegrees: 0, pointRadius: 0, item: ITEM_FOR_LINE, time: 3000, degrees: 10, intervalRadius: 50, maxRadius: 600},
-                    {type: 'line', pointDegrees: 0, pointRadius: 0, item: ITEM_FOR_LINE, time: 3000, degrees: 50, intervalRadius: 50, maxRadius: 600},
-                    {type: 'line', pointDegrees: 0, pointRadius: 0, item: ITEM_FOR_LINE, time: 6000, degrees: 180, intervalRadius: 50, maxRadius: 600},
-                    {type: 'line', pointDegrees: 0, pointRadius: 0, item: ITEM_FOR_LINE, time: 6000, degrees: 220, intervalRadius: 50, maxRadius: 600}
+                    {type: 'line', pointDegrees: 0, pointRadius: 0, item: ITEM_FOR_LINE, time: 3000, degrees: 10, intervalRadius: 50, maxRadius: 750},
+                    {type: 'line', pointDegrees: 0, pointRadius: 0, item: ITEM_FOR_LINE, time: 3000, degrees: 50, intervalRadius: 50, maxRadius: 750},
+                    {type: 'line', pointDegrees: 0, pointRadius: 0, item: ITEM_FOR_LINE, time: 6000, degrees: 180, intervalRadius: 50, maxRadius: 750},
+                    {type: 'line', pointDegrees: 0, pointRadius: 0, item: ITEM_FOR_LINE, time: 6000, degrees: 220, intervalRadius: 50, maxRadius: 750}
                 ], timeout: 0},
                 //Get Skulls
                 {id: 156, msg: '', spawns: [
                     {type: 'item', pointDegrees: 360, pointRadius: 450, item: ITEM_BOMB, time: 4000, degrees: 0, radius: 0},
-                    {type: 'item', pointDegrees: 180, pointRadius: 450, item: ITEM_FOR_CIRCLE, time: 4000, degrees: 0, radius: 0},
-                    {type: 'circle', pointDegrees: 360, pointRadius: 450, item: ITEM_BOMB, time: 4000, intervalDegrees: 20, radius: 180},
+                    {type: 'item', pointDegrees: 180, pointRadius: 450, item: ITEM_BOMB, time: 4000, degrees: 0, radius: 0},
+                    {type: 'circle', pointDegrees: 360, pointRadius: 450, item: ITEM_FOR_CIRCLE, time: 4000, intervalDegrees: 20, radius: 180},
                     {type: 'circle', pointDegrees: 180, pointRadius: 450, item: ITEM_FOR_CIRCLE, time: 4000, intervalDegrees: 20, radius: 180}
                 ], timeout: 0},
                 //Gather!
